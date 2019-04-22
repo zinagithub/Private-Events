4eae72cd685d23a272d46ff8ce0b77eb878d8f89
YellowPipe <philippdmitrienko@gmail.com>
2019-04-22 16:36:22 +0300

    added users with sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :sessions
+  get 'login', to: 'sessions#new', as: "login"
+  get 'logout', to: 'sessions#destroy', as: "logout"
+  get 'signup', to: 'users#new', as: "signup"
 end
