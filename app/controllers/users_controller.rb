5d599be7846690accb2a10def3351f80cdcbb846
Talaa Zina <talazina@gmail.com>
2019-04-26 14:59:01 +0100

    fix code review errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @events = @user.events_as_attendee
+    @created_events = @user.events
   end
 
   # GET /users/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'sessions#new'	
   resources :events
   resources :users
   resources :sessions
