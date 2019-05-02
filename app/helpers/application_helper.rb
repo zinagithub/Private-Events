339d4807e8e7bf834c29088c8c4337412f37f94d
YellowPipe <philippdmitrienko@gmail.com>
2019-05-02 17:01:57 +0300

    removed logic from views, added some links to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	def navbar_helper
+	   if logged_in? 
+         (link_to current_user.name, current_user)+' | '.html_safe+
+         (link_to 'Logout', logout_path)+' | '.html_safe+
+         (link_to 'Events', events_path)+' | '.html_safe+
+         (link_to 'Users', users_path)+' | '.html_safe+' | '+
+         (link_to 'New event', new_event_path)
+       else
+         (link_to 'Register', signup_path)+' | '.html_safe+
+         (link_to 'Login', login_path)
+       end
+	end
+
 end
